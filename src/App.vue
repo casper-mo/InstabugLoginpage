4d9889344caeb69a644f419a9214b6d5a594dc5d
CASP3R <mahmoudmu7amed@gmail.com>
2021-06-04 21:13:07 +0200

    fix errors

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <router-view />
 </template>
-
+<script>
+export default {
+  mounted: function () {
+    this.$router.replace("/login");
+  },
+};
+</script>
 <style lang="scss">
 @import "@/sass/global";
 </style>
